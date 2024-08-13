661da09928b3a87216026b947f9d0b000b127754
naimaubry <naima.aubry.usa@gmail.com>
2024-08-13 19:17:30 -0400

    Initial Commit

---
+++ EE_implementation/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  EE_implementation
+//
+//  Created by Naima Aubry on 8/13/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
